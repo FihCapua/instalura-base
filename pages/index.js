d607d56ab4397348da32beb5a4a8ead29fa50665
FihCapua <fihcapua@gmail.com>
2021-02-09 22:23:16 -0300

    test: end first class - 01

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+// Propriedade do elemento title que vem como um objeto
+// props = {
+//   children: Qualquer valor
+// }
+
+function Title(props) {
+  return <h1>{props.children}</h1>
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <div><Title>Qualquer valor</Title>Hot reload :burn.</div>
 }
