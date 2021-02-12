0058221e5132c68b71f00e6342c4603c9a9eda41
FihCapua <fihcapua@gmail.com>
2021-02-11 21:45:58 -0300

    feature: create components Head, Footer and include fonts - class 04

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,18 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import theme from '../src/theme';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
+import { GlobalStyle } from '../src/theme/GlobalStyle';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+    <Head>
+      <title>Instalura - Projeto Base</title>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+    </Head>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
