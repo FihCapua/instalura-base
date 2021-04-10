68cf7938fa78279e27dc3a284652ee497433bd71
FihCapua <fihcapua@gmail.com>
2021-04-10 18:10:42 -0300

    fix(test): fix bugs in cypress v3

--- src/components/patterns/formLogin/index.js
+++ src/components/patterns/formLogin/index.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import * as yup from 'yup';
 import { Button } from '../../commons/Button';
 import TextField from '../../forms/TextField';
 import { useForm } from '../../../infra/hooks/form/useForm';
 import { loginService } from '../../../services/login/loginService';
 
-const loginSchema = yup.object().shape({
-  usuario: yup
-    .string()
-    .required('"Usuario" é obrigatório')
-    .min(3, 'Preencha ao menos 3 caracteres'),
-  senha: yup
-    .string()
-    .required('"Senha" é obrigatória')
-    .min(8, 'Sua senha precisa ter ao menos 8 caracteres'),
-});
-
-// eslint-disable-next-line react/prop-types
-export default function LoginForm({ onSubmit }) {
+export default function LoginForm() {
   const router = useRouter();
   const initialValues = {
     usuario: '',
@@ -27,49 +14,30 @@ export default function LoginForm({ onSubmit }) {
   const form = useForm({
     initialValues,
     onSubmit: (values) => {
-      form.setIsFormDisable(true);
       loginService.login({
         username: values.usuario, // 'omariosouto'
         password: values.senha, // 'senhasegura'
       })
         .then(() => {
           router.push('/app/profile');
-        })
-        .catch((err) => {
-          // eslint-disable-next-line no-console
-          console.log(err);
-        })
-        .finally(() => {
-          form.setIsFormDisabled(false);
         });
-    },
-    async validateSchema(values) {
-      return loginSchema.validate(values, {
-        abortEarly: false,
-      });
     },
   });
 
   return (
-    <form id="formCadastro" onSubmit={onSubmit || form.handleSubmit}>
+    <form id="formCadastro" onSubmit={form.handleSubmit}>
       <TextField
         placeholder="Usuário"
         name="usuario"
         value={form.values.usuario}
-        error={form.errors.usuario}
-        isTouched={form.touched.usuario}
         onChange={form.handleChange}
-        onBlur={form.handleBlur}
       />
       <TextField
         placeholder="Senha"
         name="senha"
         type="password"
         value={form.values.senha}
-        error={form.errors.senha}
-        isTouched={form.touched.senha}
         onChange={form.handleChange}
-        onBlur={form.handleBlur}
       />
 
       <Button
@@ -80,15 +48,9 @@ export default function LoginForm({ onSubmit }) {
           md: 'initial',
         }}
         fullWidth
-        disabled={form.isFormDisabled}
       >
         Entrar
       </Button>
-
-      {/* <pre>
-        {JSON.stringify(form.touched, null, 4)}
-      </pre> */}
-
     </form>
   );
 }
